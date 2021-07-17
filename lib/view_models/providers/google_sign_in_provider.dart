e10b7585b322744b3aeae991259e4207fb7a8c71
dev01 <ahmednasser74>
2021-07-17 10:40:34 +0200

    edit google sign in method

--- lib/view_models/providers/google_sign_in_provider.dart
+++ lib/view_models/providers/google_sign_in_provider.dart
@@ -3,39 +3,40 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInProvider extends ChangeNotifier {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+  );
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  GoogleSignInAccount? _user;
-  final googleSignIn = GoogleSignIn();
+  // GoogleSignInAccount? _user;
+  // final googleSignIn = GoogleSignIn();
+  // Future<void> googleLogin() async {
+  //   final googleUser = await googleSignIn.signIn();
+  //   if (googleUser == null) return;
+  //   _user = googleUser;
+  //   final googleAuth = await googleUser.authentication;
+  //   final credential = GoogleAuthProvider.credential(
+  //     accessToken: googleAuth.accessToken,
+  //     idToken: googleAuth.idToken,
+  //   );
+  //   print(credential);
+  //   await FirebaseAuth.instance.signInWithCredential(credential);
+  //   notifyListeners();
+  // }
+
   Future<void> googleLogin() async {
-    final googleUser = await googleSignIn.signIn();
-    if (googleUser == null) return;
-    _user = googleUser;
-    final googleAuth = await googleUser.authentication;
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
+    GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+    final googleSignInAuth = await googleSignInAccount!.authentication;
+    final authCredential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuth.idToken,
+      accessToken: googleSignInAuth.accessToken,
     );
-    print(credential);
-    await FirebaseAuth.instance.signInWithCredential(credential);
-    notifyListeners();
+    final authResult = await _auth.signInWithCredential(authCredential);
+    final user = authResult.user;
+    print('user >> ${user!.email}');
+    final user2 = authResult.additionalUserInfo;
   }
 
-  // Future<void> googleSignIn() async {
-  //   GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
-  //   final googleSignInAuth = await googleSignInAccount!.authentication;
-  //   final authCredential = GoogleAuthProvider.credential(
-  //     idToken: googleSignInAuth.idToken,
-  //     accessToken: googleSignInAuth.accessToken,
-  //   );
-  //   final authResult = await _auth.signInWithCredential(authCredential);
-  //   final user = authResult.user;
-  //   print('user >> ${user!.email}');
-  //   final user2 = authResult.additionalUserInfo;
-  // }
-  //
-  // Future<void> googleSignOut() async {
-  //   await _googleSignIn.signOut();
-  // }
+  Future<void> googleSignOut() async {
+    await _googleSignIn.signOut();
+  }
 }
